bc1e43fad5b9675eed84a17b47759ebbaea91ee9
Simon Crowther <simon.crowther95@hotmail.co.uk>
2019-10-23 22:45:53 +0100

    Addition of UP updates to deal with access design topics

---
+++ RabbitMqTestUsage/NACSim.py
@@ -0,0 +1,47 @@
+'''
+Created on 10 Oct 2019
+
+@author: simon
+'''
+
+#!/usr/bin/env python
+import pika
+import sys, time
+
+###
+# Network Access Controller Simulator Interface
+# This is to show how the NAC could manager
+# its necessary pub & sub topics with rabbitmq
+
+### Setup of EVM connection
+print("## Beginning EVM")
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+channel.exchange_declare(exchange='topics', exchange_type='topic', durable=False)
+binding_key = 'access.request'
+#
+
+# Publishing
+result = channel.queue_declare('', exclusive=False, durable=True)
+#
+
+#
+queue_name = result.method.queue
+channel.queue_bind(exchange='topics', queue=queue_name, routing_key=binding_key)
+print("Waiting for request from UP")
+#
+
+def callback(ch, method, properties, body):
+    #print(" [x] %r:%r" % (method.routing_key, body))
+    str = body.decode()
+    print("UP sent us a request, we received " + str)
+    key = 'access.response'
+    print("The pin is incorrect!")
+    time.sleep(0.5)
+    print("Sending warning to UP")
+    channel.queue_bind(exchange='topics', queue=queue_name, routing_key=key)
+    channel.basic_publish(exchange='topics', routing_key=key, body='FAIL')
+    sys.exit()
+
+channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=False)
+channel.start_consuming()
---
+++ UserPanel/src/main/java/mainClass.java
@@ -0,0 +1,43 @@
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+public class mainClass
+{
+
+    private static final String EXCHANGE_NAME = "topics";
+    private static ConnectionFactory factory;
+
+    public static void main(String[] argv) throws Exception
+    {
+        factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.exchangeDeclare(EXCHANGE_NAME, "topic");
+        String queueName = channel.queueDeclare().getQueue();
+
+        String routingKey = "access.request";
+        String message = "pin:1234\n";
+
+        channel.basicPublish(EXCHANGE_NAME, routingKey, null, message.getBytes("UTF-8"));
+        System.out.println(" [x] Sent '" + routingKey + "':'" + message + "'");
+
+        channel.queueBind(queueName, EXCHANGE_NAME, "access.response");
+        System.out.println(" [*] Waiting for access.response. To exit press CTRL+C");
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String received = new String(delivery.getBody(), "UTF-8");
+            System.out.println(" [x] Received '" + delivery.getEnvelope().getRoutingKey() + "':'" + received + "'");
+            if(!received.equals("PASS"))
+            {
+                String pubRoutingKey = "event.UP";
+                String pubMessage = "We have access denied at 14:00";
+                channel.basicPublish(EXCHANGE_NAME, pubRoutingKey, null, pubMessage.getBytes("UTF-8"));
+            }
+            System.exit(1);
+        };
+        channel.basicConsume(queueName, true, deliverCallback, consumerTag -> { });
+    }
+}
