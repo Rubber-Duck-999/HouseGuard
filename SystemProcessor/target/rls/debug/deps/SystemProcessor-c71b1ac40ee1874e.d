3d6582bd8ca17602a34a0bcc59d4d4694eeb5954
Rubber-Duck-999 <simon.crowther95@hotmail.co.uk>
2019-11-22 13:46:21 +0000

    Added configuration to get project up and running, settled on using QT C++ for ESC to give more expereince

--- SystemProcessor/target/rls/debug/deps/SystemProcessor-c71b1ac40ee1874e.d
+++
@@ -1,5 +0,0 @@
-/mnt/c/Users/simon/Documents/HouseGuard/SystemProcessor/target/rls/debug/deps/SystemProcessor-c71b1ac40ee1874e.rmeta: src/main.rs
-
-/mnt/c/Users/simon/Documents/HouseGuard/SystemProcessor/target/rls/debug/deps/SystemProcessor-c71b1ac40ee1874e.d: src/main.rs
-
-src/main.rs:
--- SystemProcessor/target/rls/debug/deps/SystemProcessor-c9e29df1b42fffaf.d
+++
@@ -1,5 +0,0 @@
-/mnt/c/Users/simon/Documents/HouseGuard/SystemProcessor/target/rls/debug/deps/SystemProcessor-c9e29df1b42fffaf.rmeta: src/main.rs
-
-/mnt/c/Users/simon/Documents/HouseGuard/SystemProcessor/target/rls/debug/deps/SystemProcessor-c9e29df1b42fffaf.d: src/main.rs
-
-src/main.rs:
