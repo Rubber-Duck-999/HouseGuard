a22928a5dce9a113948d2a48a019c504e029d34f
Rubber-Duck-999 <simon.crowther95@hotmail.co.uk>
2019-11-26 10:48:22 +0000

    Added deployment ability for tag V1

--- FaultHandler/src/Pub/main.go
+++ FaultHandler/src/Pub/main.go
@@ -11,8 +11,8 @@ import (
 func main() {
 	log.SetLevel(log.TraceLevel)
 	log.Trace("FH - Beginning to run Fault Handler Program")
-	//file := "/home/ubuntu/environment/HouseGuard/FaultHandler/config.yml"
-	/*var data config.ConfigTypes
+	file := "/home/ubuntu/environment/HouseGuard/FaultHandler/config.yml"
+	var data config.ConfigTypes
 	if config.Exists(file) {
 		data := config.GetData(file)
 		log.Debug(data)
@@ -20,8 +20,8 @@ func main() {
 		os.Exit(1)
 	}
 	valid := rabbitmq.SetEmailSettings(data.EmailSettings.Email,
-	data.EmailSettings.Password,
-	data.EmailSettings.Name,
-	data.EmailSettings.To_email)*/
+		data.EmailSettings.Password,
+		data.EmailSettings.Name,
+		data.EmailSettings.To_email)
 	rabbitmq.Subscribe()
 }
