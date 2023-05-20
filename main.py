7c5c76bf1c3f8012b70b39640c83e68c2588c53c
rubber-duck-999 <simon.crowther95@hotmail.co.uk>
2023-05-20 14:03:49 +0100

    Take a look at pi zero 2 as well

--- main.py
+++ main.py
@@ -3,14 +3,28 @@ import json
 
 def getStock(URL):
     page = requests.get(URL)
-    data = json.loads(page.content)
-    if 'available' in data:
-        return True
+    if page.status_code == 200:
+        try:
+            data = json.loads(page.content)
+            if 'available' in data:
+                return True
+        except json.JSONDecodeError as error:
+            print('Error decoding json: ' + error.msg)
+            print(page.content)
     return False
 
 def getPI400():
     return getStock("https://shop.pimoroni.com/products/raspberry-pi-400-personal-computer-kit.js")
 
+def getPIZERO():
+    return getStock("https://shop.pimoroni.com/product/raspberry-pi-zero-essentials-kit.js")
+
 if __name__ == "__main__":
     if getPI400():
         print('PI 400 is in Stock at Pimoroni')
+    else:
+        print('PI 400 is not in Stock at Pimoroni')
+    if getPIZERO():
+        print('PI Zero Kit is in Stock at Pimoroni')
+    else:
+        print('PI Zero Kit is not in Stock at Pimoroni')
